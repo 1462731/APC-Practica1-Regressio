7636bad117d602ce1ff5be5f6a452beca3ad67ac
franmuline <franmb2000@gmail.com>
2021-10-17 18:23:57 +0200

    Apartat B - Regresions lineals

---
+++ regressions.py
@@ -0,0 +1,130 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import scipy.stats as sp
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.decomposition import PCA
+
+
+def return_full_data():
+    abrasan_csv = pd.read_csv('dataset/TabrizPollution/Abrasan.csv', delimiter=';')
+    bashumal_csv = pd.read_csv('dataset/TabrizPollution/Bashumal.csv', delimiter=';')
+    rastakucha_csv = pd.read_csv('dataset/TabrizPollution/RastaKucha.csv', delimiter=';')
+    data = pd.concat([abrasan_csv, bashumal_csv, rastakucha_csv], ignore_index=True)
+    df = data[(data['air_temperature'] != -9999.0) & (data['dewpoint'] != -9999.0) & (data['wind_direction_corr'] != -9999.0) &
+                (data['wind_speed'] != -9999.0) & (data['relative_pressure'] != -9999.0) & (data['PM10'] != -9999.0) &
+                (data['PM2.5'] != -9999.0)]
+    return df
+
+def mean_squeared_error(y1, y2):
+    # comprovem que y1 i y2 tenen la mateixa mida
+    assert(len(y1) == len(y2))
+    mse = 0
+    for i in range(len(y1)):
+        mse += (y1[i] - y2[i])**2
+    return mse / len(y1)
+
+def regression(x, y):
+    # Creem un objecte de regressió de sklearn
+    regr = LinearRegression()
+
+    # Entrenem el model per a predir y a partir de x
+    regr.fit(x, y)
+
+    # Retornem el model entrenat
+    return regr
+
+def mse(v1, v2):
+    return ((v1 - v2)**2).mean()
+
+def standarize(x_train):
+    mean = x_train.mean(0)
+    std = x_train.std(0)
+    x_t = x_train - mean[None, :]
+    x_t /= std[None, :]
+    return x_t
+
+
+""" Per a assegurar-nos que el model s'ajusta be a dades noves, no vistes, 
+cal evaluar-lo en un conjunt de validacio (i un altre de test en situacions reals).
+Com que en aquest cas no en tenim, el generarem separant les dades en 
+un 80% d'entrenament i un 20% de validació.
+"""
+def split_data(x, y, train_ratio=0.8):
+    indices = np.arange(x.shape[0])
+    np.random.shuffle(indices)
+    n_train = int(np.floor(x.shape[0]*train_ratio))
+    indices_train = indices[:n_train]
+    indices_val = indices[n_train:]
+    x_train = x[indices_train, :]
+    y_train = y[indices_train]
+    x_val = x[indices_val, :]
+    y_val = y[indices_val]
+    return x_train, y_train, x_val, y_val
+
+full_data = return_full_data()
+full_data = full_data.drop('Time', axis=1)
+first = full_data['air_temperature'].values
+s, p = sp.normaltest(first)
+array = np.random.normal(size=48000)
+s, p = sp.normaltest(array)
+
+
+standarized_data = standarize(full_data)
+standarized_data.hist(bins=25)
+plt.show()
+
+x = standarized_data.values[:, :6]
+y = standarized_data.values[:, 6]
+
+regr = regression(x, y)
+predicted = regr.predict(x)
+
+MSE = mse(y, predicted)
+r2 = r2_score(y, predicted)
+
+print("Mean squeared error: ", MSE)
+print("R2 score: ", r2)
+
+for i in range(0, x.shape[1]):
+    atribut = x[:,i].reshape(x.shape[0], 1)
+    regr = regression(atribut, y)
+    predicted = regr.predict(atribut)
+
+    # Mostrem la predicció del model entrenat en color vermell a la Figura anterior 1
+    plt.figure()
+    ax = plt.scatter(x[:,0], y)
+    plt.plot(atribut[:,0], predicted, 'r')
+    plt.show()
+
+    # Mostrem l'error (MSE i R2)
+    MSE = mse(y, predicted)
+    r2 = r2_score(y, predicted)
+
+    print("Mean squeared error: ", MSE)
+    print("R2 score: ", r2)
+
+# Dividim dades d'entrenament
+x_train, y_train, x_val, y_val = split_data(standarized_data.values[:, :6], standarized_data.values[:, 6])
+
+for i in range(x_train.shape[1]):
+    x_t = x_train[:,i] # seleccionem atribut i en conjunt de train
+    x_v = x_val[:,i] # seleccionem atribut i en conjunt de val.
+    x_t = np.reshape(x_t,(x_t.shape[0],1))
+    x_v = np.reshape(x_v,(x_v.shape[0],1))
+
+    regr = regression(x_t, y_train)
+    error = mse(y_val, regr.predict(x_v)) # calculem error
+    r2 = r2_score(y_val, regr.predict(x_v))
+
+    print("Error en atribut %d: %f" %(i, error))
+    print("R2 score en atribut %d: %f" %(i, r2))
+
+
+pca = PCA(n_components='mle')
+x_train = pca.fit_transform(x)
+
+
+
+
