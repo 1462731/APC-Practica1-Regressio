7d2716768f932bb6b73b23ab98d15d48076a6b44
franmuline <franmb2000@gmail.com>
2021-10-27 17:31:29 +0200

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 from sklearn.metrics import r2_score
+import copy
 
 
 def return_full_data():
@@ -67,7 +68,7 @@ class Regressor(object):
 
         pass
 
-    def train(self, x, y, iterations: int, epsilon):
+    def train(self, x, y, iterations: int, epsilon=0):
         # Entrenar durant max_iter iteracions o fins que la millora sigui inferior a epsilon
         cost_history = np.zeros(iterations, dtype=object)
         m = len(y)
@@ -77,8 +78,53 @@ class Regressor(object):
             self.w0 = self.w0 - self.alpha/m *np.sum(gradient)
             for j in range(len(self.theta)):
                 final_gradient = gradient * x[:, j]
-                self.theta[j] = self.theta[j] - alpha/m * np.sum(final_gradient)
+                self.theta[j] = self.theta[j] - self.alpha/m * np.sum(final_gradient)
+            cost_history[it] = self.cost(x, y)
         return self.theta, cost_history
+
+
+def testing(x_train, y_train, x_val, y_val, alphas, theta, w0, iterations):
+    cost_history = []
+    meanr2s = []
+    theta1 = 0
+    for j in range(len(alphas)):
+        alpha = alphas[j]
+        theta1 = copy.deepcopy(theta)
+        regr = Regressor(theta1, alpha, w0)
+        # y_train = np.zeros(4)
+        theta1, cost = regr.train(x_train, y_train, iterations)
+        cost_history.append(cost)
+        print('Final value of theta =', theta1)
+        print('First 5 values from cost_history =', cost[:5])
+        print('Last 5 values from cost_history =', cost[-5:])
+
+        acc = 0
+        for i in range(len(y_val)):
+            y_pred = regr.predict(x_val)
+            r2score = r2_score(y_val, y_pred)
+            acc = acc + r2score
+        meanr2s.append(acc / len(y_val))
+        print(meanr2s[j])
+
+    plt.figure(1)
+    for j in range(len(alphas)):
+        plt.plot(range(1, iterations + 1), cost_history[j], label='alpha =%.5f' % alphas[j])
+    plt.grid()
+    plt.xlabel("Nombre iteracions")
+    plt.ylabel("Cost (J)")
+    plt.legend(bbox_to_anchor=(1.04, 0.4), loc="upper left")
+    plt.title("Reducció del cost - {} iteracions".format(iterations), y=1.08)
+    plt.show()
+
+    plt.figure(2)
+    plt.bar(['0.0001', '0.0005', '0.001', '0.005', '0.01', '0.05'], meanr2s)
+    plt.grid()
+    plt.ylabel('r2 score')
+    plt.xlabel('alpha value')
+    plt.ylim([0.0, 1.0])
+    plt.title("r2 score per learning rate - {} iteracions".format(iterations), y=1.08)
+    plt.show()
+    return 0
 
 
 # Obtenim les dades netes i les estandaritzem
@@ -95,46 +141,8 @@ y = standarized_data.values[:, 5]
 
 x_train, y_train, x_val, y_val = split_data(standarized_data.values[:, :5], standarized_data.values[:, 5])
 
-# Definim els valors per alpha i theta (conjunt de pesos) per fer el descens de gradient
-alpha = 0.0001
-theta = np.random.uniform(0.0, 1.0, size=5)
+thetass = np.random.uniform(0.0, 1.0, size=5)
 w0 = np.random.uniform(0.0, 1.0)
-iterations = 400
-regr = Regressor(theta,alpha, w0)
-np.set_printoptions(precision=4)
-
-# Fem el descens de gradient
-theta, cost_history = regr.train(x_train, y_train, iterations, 0 )
-print('Final value of theta =', theta)
-print('First 5 values from cost_history =', cost_history[:5])
-print('Last 5 values from cost_history =', cost_history[-5 :])
-
-# Avaluem el resultat
-acc = 0
-y_pred = regr.predict(x_val)
-r2score = r2_score(y_val, y_pred)
-print(r2score)
-
-# Provem amb alpha = 0.001
-alpha = 0.001
-theta, cost_history = regr.train(x_train, y_train, iterations, 0 )
-print('Final value of theta =', theta)
-print('First 5 values from cost_history =', cost_history[:5])
-print('Last 5 values from cost_history =', cost_history[-5 :])
-acc = 0
-y_pred = regr.predict(x_val)
-r2score = r2_score(y_val, y_pred)
-print(r2score)
-
-
-# Provem amb alpha 0.01
-alpha = 0.1
-#y_train = np.zeros(4)
-theta, cost_history = regr.train(x_train, y_train, iterations, 0 )
-print('Final value of theta =', theta)
-print('First 5 values from cost_history =', cost_history[:5])
-print('Last 5 values from cost_history =', cost_history[-5 :])
-y_pred = regr.predict(x_val)
-r2score = r2_score(y_val, y_pred)
-print(r2score)
-
+alphas = [0.0001, 0.0005, 0.001,0.005,0.01, 0.05]
+testing(x_train, y_train,x_val, y_val, alphas, thetass, w0, 400)
+testing(x_train, y_train,x_val, y_val, alphas, thetass,w0, 1000)
